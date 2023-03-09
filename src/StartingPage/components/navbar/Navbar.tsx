e708a3e65d19e74ad21a1ac8064193eeae62a997
KrzysztofWolniak <Krzysztof.Wolniak01@gmail.com>
2023-03-09 15:26:11 +0100

    first commit

---
+++ src/StartingPage/components/navbar/Navbar.tsx
@@ -0,0 +1,120 @@
+import { Fragment, useState } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+
+//TODO zaimplementować pobieranie danych z backendu
+const navigation = [
+  { name: "Dashboard", href: "#", current: true },
+  { name: "Team", href: "#", current: false },
+  { name: "Projects", href: "#", current: false },
+  { name: "Calendar", href: "#", current: false },
+  { name: "Zamów", href: "#", current: false },
+];
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export function Navbar() {
+  const [navigationItems, setNavigationItems] = useState(navigation);
+  //Changing changing current activ
+  const updateLinksStatus = (index: number) => {
+    //debugger;
+    const tempNavigationItems = [...navigationItems];
+
+    tempNavigationItems.every((Element) => {
+      if (Element.current) {
+        Element.current = false;
+        return false;
+      }
+      else return true;
+    });
+
+    tempNavigationItems[index].current = true;
+    return tempNavigationItems;
+  };
+
+  return (
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 md:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between ">
+              <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start last:ml-auto ">
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="block h-8 w-auto lg:hidden"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company"
+                  />
+                  <img
+                    className="hidden h-8 w-auto lg:block"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company"
+                  />
+                </div>
+                <div className="hidden md:ml-auto md:block">
+                  <div className="flex space-x-4">
+                    {navigationItems.map((item, index) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? "bg-gray-900 text-white"
+                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                          "rounded-md px-3 py-2 text-md font-medium"
+                        )}
+                        aria-current={item.current ? "page" : undefined}
+                        onClick={() =>
+                          setNavigationItems(updateLinksStatus(index))
+                        }
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="md:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigationItems.map((item,index) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block rounded-md px-3 py-2 text-base font-medium"
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                  onClick={() =>
+                    setNavigationItems(updateLinksStatus(index))
+                  }
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  );
+}
